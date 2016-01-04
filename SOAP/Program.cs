93ea8cdaf9c92ff261afd0d6dbe58c3d227a2695
patr.d <patr.d@mail.ru>
2016-01-04 23:25:23 +0300

    new

---
+++ SOAP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace SOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new Service(new Function());
+            var webService = service.OpenService("http://localhost:8811/");
+            Console.ReadKey();
+            service.CloseService(webService);
+        }
+
+        public class Function : Service.IService
+        {
+            public string Execute()
+            {
+                Console.WriteLine(OperationContext.Current.RequestContext.RequestMessage.ToString());
+                return "SOAP";
+            }
+        }
+
+        public class Service
+        {
+            private readonly IService _function;
+
+            public Service(IService service)
+            {
+                this._function = service;
+            }
+
+            [ServiceContract]
+            public interface IService
+            {
+                [OperationContract]
+                string Execute();
+            }
+
+            public WebServiceHost OpenService(string address)
+            {
+                var uri = new Uri(address);
+                WebServiceHost service = new WebServiceHost(_function.GetType(), uri);
+                service.AddServiceEndpoint(typeof(IService), new BasicHttpBinding(), String.Empty);
+                service.Open();
+                return service;
+            }
+
+            public void CloseService(WebServiceHost service) => service?.Close();
+
+        }
+    }
+}
---
+++ SOAP_Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace SOAP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = RequireСontract("http://localhost:8811");
+            Console.WriteLine(service.Execute());
+            Console.ReadKey();
+        }
+
+        private static IService RequireСontract(string address)
+        {
+            var endpoint = new EndpointAddress(address);
+            ChannelFactory<IService> channel = new ChannelFactory<IService>(new BasicHttpBinding(), endpoint);
+            return channel.CreateChannel();
+        }
+        [ServiceContract]
+        public interface IService
+        {
+            [OperationContract]
+            string Execute();
+        }
+
+
+    }
+}
